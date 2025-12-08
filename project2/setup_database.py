3ac21ddda7873f5eed038c6b94764d6909903757
Abdelkerim Doutoum <AD1891@live.mdx.ac.uk>
2025-12-08 11:10:13 +0400

    changes

--- project2/setup_database.py
+++
@@ -1,6 +0,0 @@
-from database.db import DatabaseInitializer
-
-if __name__ == "__main__":
-    print("Setting up database...")
-    DatabaseInitializer().initialize()
-    print("Database setup complete!")
